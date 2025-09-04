90ed4dd61f8005d9bf4d15062928dc46fa09b758
joaopauloaramuni <joaopauloaramuni>
2025-09-04 17:29:48 -0300

    portfolio-v4

--- src/emailjsConfig.js
+++ src/emailjsConfig.js
@@ -1,8 +1,8 @@
 // https://dashboard.emailjs.com/admin
 const EMAILJS_CONFIG = {
-  SERVICE_ID: "seu_service_id_aqui", //seu_service_id_aqui
-  TEMPLATE_ID: "seu_template_id_aqui", //seu_template_id_aqui
-  PUBLIC_KEY: "seu_public_key_aqui", //seu_public_key_aqui
+  SERVICE_ID: import.meta.env.VITE_EMAILJS_SERVICE_ID,
+  TEMPLATE_ID: import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+  PUBLIC_KEY: import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
 };
 
 export default EMAILJS_CONFIG;
