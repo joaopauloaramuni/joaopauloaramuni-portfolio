f05e4a6e39102e1363aa792136fce095269b6b69
João Paulo Aramuni <joaopauloaramuni@gmail.com>
2025-09-07 14:17:09 -0300

    Delete src/config directory

--- src/config/GitHubAPIConfig.js
+++
@@ -1,9 +0,0 @@
-// gitHubApiConfig.js
-const GITHUB_API_CONFIG = {
-  USERNAME: "joaopauloaramuni",
-  TOKEN: import.meta.env.VITE_GITHUB_TOKEN,
-  BASE_URL: "https://api.github.com",
-  PER_PAGE: 100, // quantidade máxima de repositórios por página
-};
-
-export default GITHUB_API_CONFIG;
