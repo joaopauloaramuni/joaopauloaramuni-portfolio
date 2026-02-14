c413f46948bde68145c560db756568f3b24997b6
joaopauloaramuni <joaopauloaramuni>
2026-02-13 21:41:17 -0300

    portfolio-v5

--- src/components/WakaTime.jsx
+++ src/components/WakaTime.jsx
@@ -7,10 +7,20 @@ const WakaTime = () => {
       <table>
         <tr>
           <td>
-            <img src="https://helio-github-stats.vercel.app/api/wakatime?username=aramuni&custom_title=WakaTime+Stats&card_width=466&line_height=25&layout=compact&display_format=time&disable_animations=false&langs_count=22" alt="WakaTime Stats" width="466" loading="lazy"/>
+            <img
+              src="https://helio-github-stats.vercel.app/api/wakatime?username=aramuni&custom_title=WakaTime+Stats&card_width=466&line_height=25&layout=compact&display_format=time&disable_animations=false&langs_count=22"
+              alt="WakaTime Stats"
+              width="466"
+              loading="lazy"
+            />
           </td>
           <td>
-            <img src="https://helio-github-stats.vercel.app/api/wakatime?username=aramuni&custom_title=WakaTime+Stats&card_width=466&line_height=25&layout=default&display_format=time&disable_animations=false&langs_count=12" alt="WakaTime Stats" width="466" loading="lazy"/>
+            <img
+              src="https://helio-github-stats.vercel.app/api/wakatime?username=aramuni&custom_title=WakaTime+Stats&card_width=466&line_height=25&layout=default&display_format=time&disable_animations=false&langs_count=12"
+              alt="WakaTime Stats"
+              width="466"
+              loading="lazy"
+            />
           </td>
         </tr>
       </table>
