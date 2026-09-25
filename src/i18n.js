3268a92cafc0720f93425ba0970a4eaecedeb259
joaopauloaramuni <joaopauloaramuni>
2026-09-25 07:43:43 -0300

    portfolio-v6

--- src/i18n.js
+++ src/i18n.js
@@ -233,10 +233,10 @@ const resources = {
         nenhum: "No recommendations yet",
 
         rec71_relationship:
-        "On September 25, 2026, Fernando was a client of João Paulo.",
+          "On September 25, 2026, Fernando was a client of João Paulo.",
 
         rec71_recommendation:
-        "One of the best professors I have ever had. He is truly someone who not only understands the tools he uses and teaches, but also genuinely enjoys them. His teaching style and understanding of the topics are always impressive and consistently help students put things into context and learn.",
+          "One of the best professors I have ever had. He is truly someone who not only understands the tools he uses and teaches, but also genuinely enjoys them. His teaching style and understanding of the topics are always impressive and consistently help students put things into context and learn.",
 
         rec70_relationship:
           "On June 26, 2026, Eric was responding directly to João Paulo.",
